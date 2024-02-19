b6d9b1081637364a44ae0d8ee05339d51dd5a9e6
lzk <1259787536@qq.com>
2024-02-19 17:57:54 +0800

    首页小修订

--- index/static/index/js/index.js
+++ index/static/index/js/index.js
@@ -1,37 +1,68 @@
-var chartDom = document.getElementById('heart_rate');
-var heart_rate_echarts = echarts.init(chartDom);
-var heart_rate_option;
+// 设置echarts图的方法
+function set_echarts(id) {
+  let chartDom = document.getElementById(id);
+  let mycharts = echarts.init(chartDom);
+  let option;
 
-heart_rate_option = {
-  tooltip: {
-    formatter: '{a} <br/>{b} : {c}bpm'
-  },
-  series: [
-    {
-      name: '心率',
-      type: 'gauge',
-      min: 30,
-      max: 150,
-      progress: {
-        show: true,
-      },
-      detail: {
-        valueAnimation: true,
-        formatter: '{value} bpm'
-      },
-      axisLabel: {
-        formatter: function(value) {
-          return Math.round(value); // 四舍五入到最近的整数
-        }
-      },
-      data: [
-        {
-          value: 150,
-          name: '心率'
-        }
-      ]
-    }
-  ]
-};
+  // 获取数据
+  let name = chartDom.dataset.name;
+  let min = chartDom.dataset.min;
+  let max = chartDom.dataset.max;
+  let unit = chartDom.dataset.unit;
+  let currentValue = chartDom.dataset.currentValue;
+  let isInt = chartDom.dataset.isInt === 'true';
+  let axisLabel;
 
-heart_rate_option && heart_rate_echarts.setOption(heart_rate_option);
+  // 判断该指标整型/浮点型
+  if (isInt) {
+    axisLabel = {
+      formatter: function(value) {
+        return Math.round(value); // 四舍五入到最近的整数
+      }
+    };
+  } else {
+    axisLabel = {
+      formatter: function(value) {
+        return value.toFixed(1); // 保留一位小数
+      }
+    };
+  }
+
+  option = {
+    tooltip: {
+      formatter: '{a} <br/>{b} : {c}bpm'
+    },
+    series: [
+      {
+        name: name,
+        type: 'gauge',
+        min: min,
+        max: max,
+        progress: {
+          show: true,
+        },
+        detail: {
+          valueAnimation: true,
+          formatter: '{value}' + unit,
+        },
+        axisLabel: axisLabel,
+        data: [
+          {
+            value: currentValue,
+            name: name
+          }
+        ]
+      }
+    ]
+  };
+  option && mycharts.setOption(option);
+}
+
+// 心率
+set_echarts("heart_rate");
+// 血压
+set_echarts("blood_pressure");
+// 体温
+set_echarts("temperature");
+// 血氧
+set_echarts("blood_oxygen");
